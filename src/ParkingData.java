219f388aae7784a711585ec844e0509ed2442ee1
mahuanfy <mahuanfy@foxmail.com>
2019-04-23 22:48:01 +0800

    Filish parking

---
+++ src/ParkingData.java
@@ -0,0 +1,12 @@
+import entity.Place;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ParkingData {
+    public static List<Place> places = new ArrayList<>();
+
+    public List<Place> getPlaces() {
+        return places;
+    }
+}
---
+++ src/entity/Car.java
@@ -0,0 +1,20 @@
+package entity;
+
+public class Car {
+    private String numberPlates;
+
+    public Car() {
+    }
+
+    public Car(String numberPlates) {
+        this.numberPlates = numberPlates;
+    }
+
+    public String getNumberPlates() {
+        return numberPlates;
+    }
+
+    public void setNumberPlates(String numberPlates) {
+        this.numberPlates = numberPlates;
+    }
+}
